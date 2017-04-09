41a4334fd67bcbf0b4cf4b075d0701b3bb708dbd
PraveenAnil <praveendosth@gmail.com>
2017-04-09 20:42:36 +0530

    Update installpuppetagent.sh

--- scripts/installpuppetagent.sh
+++ scripts/installpuppetagent.sh
@@ -2,6 +2,7 @@
 puname="puppet"
 sed -i "2i$1 $2" /etc/hosts
 sed -i "2i$1 $puname" /etc/hosts
-http://pm.puppetlabs.com/puppet-agent/2016.1.2/1.4.2/repos/deb/wily/PC1/puppet-agent_1.4.2-1wily_amd64.deb
+wget http://pm.puppetlabs.com/puppet-agent/2016.1.2/1.4.2/repos/deb/wily/PC1/puppet-agent_1.4.2-1wily_amd64.deb
+dpkg -i puppet-agent_1.4.2*
 apt-get install puppet -y
 puppet resource service puppet ensure=running enable=true
